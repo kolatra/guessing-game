5b94f874d5e27a3c4708ad64a77a1b489af130ab
tyler <t.louks88@gmail.com>
2022-02-24 16:58:36 -0700

    get input

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    println!("input your guess");
+    let mut guess = String::new();
+    io::stdin()
+        .read_line(&mut guess)
+        .expect("failed to read the line");
+    println!("you guessed: {}", guess);
 }
